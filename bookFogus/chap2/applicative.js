137d94630d63c865d1d174966bdba0ed66c04f15
Zihan Zhang <zzhlovesly@outlook.com>
2020-06-21 01:06:41 +0800

    what a fucking month

--- bookFogus/chap2/applicative.js
+++ bookFogus/chap2/applicative.js
@@ -1,3 +1,6 @@
+const _ = require('lodash');
+const R = require('ramda');
+const { join } = require('lodash');
 /**
  * reduceRight
  * find
@@ -6,3 +9,30 @@
  * any
  * sortBy, groupBy, countBy
  */
+
+// 你说 lodash 和 ramda 实现有什么优劣吗？
+// 1. reduceRight
+function allOf() {
+    return _.reduceRight(arguments, function(truth, f) {
+        return truth && f();
+    }, true);
+}
+
+function anyOf() {
+    return _.reduceRight(arguments, function(truth, f) {
+        return truth || f();
+    }, false);
+}
+
+// 2. reject
+function complement(pred) {
+    return function() {
+        // _.arguments ? 没必要吧，arguments 本来就是类数组
+        return !pred.apply(null, arguments);
+    }
+}
+
+// 利用 complement，反向实现 reject
+_.filter(['a', 'b', 'c', 3], complement(_.isNumber));
+
+// 3. sortBy, groupBy, countBy
